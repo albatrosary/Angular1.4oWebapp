4e9ed23afeda36d1913c58f4e717984a5bbfd292
Fumio SAGAWA <fumio.sagawa@gmail.com>
2015-05-16 13:33:57 +0900

    change StorageService

--- app/components/home/home.js
+++ app/components/home/home.js
@@ -1,18 +1,18 @@
 (function () {
   'use strict';
 
-  function HomeController(storage) {
-    this.storage = storage;
-    this.todos = storage.getTodo();
+  function HomeController(StorageService) {
+    this.StorageService = StorageService;
+    this.todos = StorageService.getTodo();
   }
 
   HomeController.prototype.addTodo = function () {
-    this.todos = this.storage.addTodo(this.todo);
+    this.todos = this.StorageService.addTodo(this.todo);
     this.todo = '';
   };
 
   HomeController.prototype.removeTodo = function (index) {
-    this.todos = this.storage.removeTodo(index);
+    this.todos = this.StorageService.removeTodo(index);
   };
 
   angular.module('app.home', [
