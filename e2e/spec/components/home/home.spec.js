1e5be0b98e85feeab462fa10f223b66c93525532
Fumio SAGAWA <fumio.sagawa@gmail.com>
2015-05-26 09:17:43 +0900

    change e2e test

--- e2e/spec/components/home/home.spec.js
+++ e2e/spec/components/home/home.spec.js
@@ -11,26 +11,5 @@ describe('Home', function () {
     
     var todoList = element.all(by.repeater('todo in home.todos'));
     expect(todoList.count()).toEqual(1);
-//    expect(todoList.last().getText()).toEqual('write a protractor test');
-    var todo = todoList.get(0);
-    var text = todo.element(by.binding('todo'));
-    expect(text).toEqual('write a protractor test');
-    //var text = todo.element(by.binding('todo.text'));
-    //expect(todo).toEqual('write a protractor test');
-
-    // getText をinputタグのvalueを取りたい
-
-//         describe('angularjs homepage todo list', function() {
-//   it('should add a todo', function() {
-//     browser.get('http://www.angularjs.org');
-
-//     element(by.model('todoText')).sendKeys('write a protractor test');
-//     element(by.css('[value="add"]')).click();
-
-//     var todoList = element.all(by.repeater('todo in todos'));
-//     expect(todoList.count()).toEqual(3);
-//     expect(todoList.get(2).getText()).toEqual('write a protractor test');
-//   });
-// });
-    });
+  });
 });
